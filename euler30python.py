4f3e9a44f49bb0ceca6039c2370e1658a76095d8
Francisco Rafart <francisco.rafart@gmail.com>
2018-09-08 14:33:03 -0400

    refactor

--- euler30python.py
+++ euler30python.py
@@ -1,15 +1,18 @@
+#List with all the characters of a string
 def map_string(s):
     res = list()
     for ss in s:
         res.append(ss)
     return res
 
+#returns the sum of all the digits of x to the power of y
 def mult_n_pow(x,y):
     pows = map(lambda ch: int(ch)**int(y),map_string(x))
     reduced = reduce(lambda r,s: r+s, pows)
 
     return str(reduced)
 
+#determines if x is equal to the power of its digits to y
 def is_pow(x,y):
     xs = str(x)
     ys = str(y)
@@ -17,12 +20,14 @@ def is_pow(x,y):
         return True
     return False
 
+#List with all numbers in range of a number
 def range_list(n):
     res = list()
     for i in range(1,n+1):
         res.append(str(i))
     return res
 
+#add two numbers as strings
 def add_two(x,y):
     return int(x)+int(y)
 
